c7076da64900d54ee364bc2ec98542d5dda27250
Victor Yodono <suunake@gmail.com>
2025-08-19 17:28:00 -0300

    feat: adiciona backend spring + JDBC + postgreSQL

---
+++ backend/src/main/java/com/each/eirv/cinema_each/CinemaEachApplication.java
@@ -0,0 +1,13 @@
+package com.each.eirv.cinema_each;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaEachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaEachApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/each/eirv/cinema_each/CinemaEachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.each.eirv.cinema_each;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaEachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
