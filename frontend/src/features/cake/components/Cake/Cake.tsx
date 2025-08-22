33050dd525fa71b4506007f6cf04c59ebc4f9d9f
Victor Yodono <suunake@gmail.com>
2025-08-22 11:49:08 -0300

    feat: adiciona biblioteca de componentes shadcn

--- frontend/src/features/cake/components/Cake/Cake.tsx
+++ frontend/src/features/cake/components/Cake/Cake.tsx
@@ -1,10 +1,22 @@
+import { Button } from "@/components/ui/button";
 import { useQueryCake } from "../../api/useQueryCake";
+import { useState } from "react";
 
 function Cake() {
   const query = useQueryCake();
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
 
   return (
-    <h1 className="font-bold underline">{query.data?.name ?? "Sem bolo :("}</h1>
+    <div>
+      <h1 className="font-bold underline">
+        {query.data?.name ? `${query.data?.name} (${count})` : "Sem bolo :("}
+      </h1>
+      <Button onClick={increment}>+ Bolo</Button>
+    </div>
   );
 }
 
