298b308649a79064e531cf4088bef0dab8318206
Victor Yodono <suunake@gmail.com>
2025-11-25 16:32:07 -0300

    fix: corrige background ao scrollar

--- frontend/src/routes/app/route.tsx
+++ frontend/src/routes/app/route.tsx
@@ -39,7 +39,7 @@ function AppLayout() {
         <div className="flex min-h-screen w-full">
           <AppSidebar />
           <main className="flex-1 p-4">
-            <GradientBackground className="absolute -z-10 inset-0 flex items-center justify-center rounded-xl from-gray-900 via-gray-700/80 to-black" />
+            <GradientBackground className="fixed -z-10 inset-0 flex items-center justify-center rounded-xl from-gray-900 via-gray-700/80 to-black" />
 
             <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
               <div className="flex items-center gap-2 px-4">
