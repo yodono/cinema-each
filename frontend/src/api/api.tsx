a8e42dc208f822dcc646a45d2062eca6683bf002
Victor Yodono <suunake@gmail.com>
2025-11-26 16:05:03 -0300

    fix: add ngrok skip browser header

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -1,6 +1,10 @@
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export async function get(path: string) {
-  const res = await fetch(`${BASE_URL}${path}`);
+  const res = await fetch(`${BASE_URL}${path}`, {
+    headers: {
+      "ngrok-skip-browser-warning": "true",
+    },
+  });
   return res.json();
 }
